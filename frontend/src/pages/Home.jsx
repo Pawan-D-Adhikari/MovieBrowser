ff03b453047659281156cc93908136d921773a0e
Pawan <pawan.dhojadhikari@gmail.com>
2026-03-01 13:50:13 +0545

    other movies grid layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -162,7 +162,7 @@ function Home() {
               </p>
             }
           >
-            <div className="flex flex-wrap gap-1">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8 gap-3 md:gap-4">
               {otherMovies.map((movie) => (
                 <div
                   key={movie.id}
